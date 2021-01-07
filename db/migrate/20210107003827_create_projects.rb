1eb79388141f16165f08a8411445b64f1664942c
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-01-06 21:58:13 -0300

    add project model

---
+++ db/migrate/20210107003827_create_projects.rb
@@ -0,0 +1,15 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.integer :status
+      t.float :mean_total
+      t.float :standard_deviation
+      t.float :actual_duration
+      t.date :started_at
+      t.date :ended_at
+
+      t.timestamps
+    end
+  end
+end
