8c5f8d531bec6c4e007af7671297b822a5a0cc69
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-01-18 22:16:46 -0300

    Add tasks validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,21 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :description, presence: true
+  validates :optimistic, presence: true
+  validates :most_likely, presence: true
+  validates :pessimistic, presence: true
+
+  after_validation :set_mean
+  after_validation :set_standard_deviation
+
+  private
+
+  def set_mean
+    self.mean = (optimistic + 4*most_likely + pessimistic)/6
+  end
+
+  def set_standard_deviation
+    self.standard_deviation = (pessimistic - optimistic)/6
+  end
 end
