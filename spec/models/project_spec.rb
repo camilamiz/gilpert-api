992edc027b83abcabbc8ebbd27ee83dfc16dd3ee
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-01-24 10:57:59 -0300

    add fixtures and change project spec to use them

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,66 +3,63 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  subject(:project) { described_class.new(attributes) }
+  fixtures :projects
+  subject(:project) { projects(:project) }
+  fixtures :tasks
 
   describe 'Validations' do
     describe 'name' do
       context 'when name is not sent' do
-        let(:attributes) { { name: nil } }
-
         it 'requires a name' do
+          project.name = nil
           expect(project).to be_invalid
           expect(project.errors.details[:name]).to include(a_hash_including(error: :blank))
         end
       end
 
       context 'when name is sent' do
-        let(:attributes) { { name: 'Groundhog project' } }
-
-        it 'does not save if name is already taken' do
-          project.save
+        it 'is not valid if name is already taken' do
           project_with_same_name = project.dup
 
-          expect { project_with_same_name.save! }.to raise_error(ActiveRecord::RecordInvalid)
+          expect(project_with_same_name).to be_invalid
+          expect(project_with_same_name.errors.messages[:name]).to eq(["has already been taken"])
         end
 
-        it 'saves if name is unique' do
-          expect{ project.save }.to change{ Project.count }.by(1)
-        end
-      end
-    end
-  end
-
-  describe 'Defaults' do
-    context 'when no attributes are given' do
-      let(:attributes) { {} }
-
-      it 'defines status as draft before validation' do
-        expect { project.valid? }
-          .to change { project.status }
-          .from(nil).to('draft')
-      end
-    end
-
-    context 'when attributes are given' do
-      context 'and it is a status' do
-        let(:attributes) { { status: :in_progress } }
-
-        it 'defines status as in_progress' do
-          expect(project.status).to eq('in_progress')
+        it 'is valid if name is unique' do
+          project_with_different_name = project.dup
+          project_with_different_name.name = 'Back to the future'
+          expect(project_with_different_name).to be_valid
         end
       end
     end
   end
 
   describe 'Associations' do
-    let(:attributes) {
-      {
-        name: 'Groundhog project',
-        status: :in_progress
-      }
-    }
+    context 'has many asks' do
+      it { should respond_to(:tasks) }
+    end
+  end
 
-    it { should have_many(:tasks) }
+  describe 'Defaults' do
+    let(:new_project) { described_class.new(attributes) }
+
+    context 'when no attributes are given' do
+      let(:attributes) { {} }
+
+      it 'defines status as draft before validation' do
+        expect { new_project.valid? }
+          .to change { new_project.status }
+          .from(nil).to('draft')
+      end
+    end
+
+    context 'when attributes are given' do
+      context 'and it is a status' do
+        it 'defines status as in_progress' do
+          project.in_progress!
+          expect(project.status).to eq('in_progress')
+        end
+      end
+    end
   end
 end
