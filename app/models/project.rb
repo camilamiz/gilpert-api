d0bce517fe40f524b90cf4c088896ae27ddae0c8
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-02-03 21:00:27 -0300

    WIP

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,14 +1,23 @@
 class Project < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
   has_many :tasks
 
   enum status: { draft: 0, in_progress: 1, finished: 2 }
 
   before_validation :fill_default_values, on: :create
 
+  validates :name, presence: true, uniqueness: true
+  validate :mean_total
+
+
   private
 
   def fill_default_values
     self.status ||= self.class.statuses[:draft]
   end
+
+  # def mean_total
+  #   if self.mean_total.nil? && (self.status == 'in_progress' || self.status == 'finished')
+  #     errors.add(:mean_total, 'must not be nil when status is in_progress or finished')
+  #   end
+  # end
 end
