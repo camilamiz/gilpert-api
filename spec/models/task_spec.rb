58172d4f5b905106aef0f7759eb8d6cb8ba7287e
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-02-03 21:27:58 -0300

    task spec WIP

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,32 +4,57 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   subject(:task) { described_class.new(attributes) }
+  let(:project) { Project.create(
+      name: 'Groundhog Project',
+      status: 1
+    ) }
 
   describe 'Validations' do
-    describe 'description' do
-      context 'when description is not sent' do
-        let(:attributes) { { description: nil } }
+    context 'when all required fields are sent' do
+      let(:attributes) {
+        {
+          description: 'Groundhog task',
+          optimistic: 3,
+          most_likely: 5,
+          pessimistic: 7,
+          project_id: project.id
+        }
+      }
 
-        it 'requires a description' do
-        end
-      end
-
-      context 'when description is sent' do
-        let(:attributes) { { description: 'Code model specs' } }
-
-        it 'saves the task' do
-
-        end
+      it 'accepts the task' do
+        task.valid?
+        expect(task.errors.details).to be_empty
       end
     end
-  end
 
-  describe 'Associations' do
-    it 'belongs to a project' do
+  #   context 'when description is not sent' do
+  #     let(:attributes) { { description: nil } }
 
-    end
+  #     it 'requires a description' do
+  #       debugger
+  #       expect(task).to be_invalid
+  #       expect(task.errors.details[:description]).to include(a_hash_including(error: :blank))
+  #     end
+  #   end
 
-  end
+  #     context 'when description is sent' do
+  #       let(:attributes) { { description: 'Groundhog task' } }
 
+  #       it 'saves the task' do
+  #         debugger
 
+  #       end
+  #     end
+  # end
+
+  # describe 'Associations' do
+  #   it 'belongs to a project' do
+
+  #   end
+
+  # end
+
+  # describe 'Defaults' do
+
+  # end
 end
