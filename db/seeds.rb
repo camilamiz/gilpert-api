341300d61bbb754e2d9556a33dd3cbcfa61d8027
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-03-07 12:07:39 -0300

    add seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+project = Project.create(
+  name: 'Project1',
+  started_at: Date.today
+)
+
+project1 = Project.create(
+  name: 'Project2',
+  started_at: Date.today - 2,
+  status: 1
+)
+
+project2 = Project.create(
+  name: 'Project2',
+  started_at: Date.today - 2,
+  status: 1
+)
+
+task1 = Task.create(
+  description: 'This is task #1',
+  optimistic: 2,
+  most_likely: 5,
+  pessimistic: 8,
+  started_at: Date.today,
+  project_id: project.id
+)
+
+# a = Task.create(description: 'This is task #2', optimistic: 1, most_likely: 3, pessimistic: 5, started_at: Date.today, project_id: 3)
+
+task2 = Task.create(
+  description: 'This is task #2',
+  optimistic: 1,
+  most_likely: 3,
+  pessimistic: 5,
+  started_at: Date.today,
+  project_id: project.id
+)
+
+task3 = Task.create(
+  description: 'This is task #3',
+  optimistic: 10,
+  most_likely: 11,
+  pessimistic: 12,
+  started_at: Date.today,
+  project_id: project1.id
+)
+
+task4 = Task.create(
+  description: 'This is task #1',
+  optimistic: 2,
+  most_likely: 5,
+  pessimistic: 8,
+  started_at: Date.today,
+  project_id: project2.id
+)
