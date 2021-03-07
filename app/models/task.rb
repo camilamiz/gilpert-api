b5d0d9325e92c28dbc191d6914751b620f59ef79
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-03-07 12:13:53 -0300

    attr_accessor was being used in a wrong way

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,17 +1,15 @@
 class Task < ApplicationRecord
-  attr_accessor :description, :optimistic, :most_likely, :pessimistic, :mean, :standard_deviation
-
   belongs_to :project
 
   validates :description, :optimistic, :most_likely, :pessimistic, presence: true
-  before_validation :sets_defaults, on: :create
+  before_validation :fill_default_values, on: :create
 
   private
 
-  def sets_defaults
-    if self.optimistic.present? && self.most_likely.present? && self.pessimistic.present?
-      self.mean ||= ((self.optimistic + 4*self.most_likely + self.pessimistic)/6).floor(2)
-      self.standard_deviation ||= ((self.pessimistic - self.optimistic)/6).floor(2)
-    end
+  def fill_default_values
+    return if (optimistic.blank? || most_likely.blank? || pessimistic.blank?)
+
+    self.mean ||= ((self.optimistic + 4*self.most_likely + self.pessimistic)/6).floor(2)
+    self.standard_deviation ||= ((self.pessimistic - self.optimistic)/6).floor(2)
   end
 end
